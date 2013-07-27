219c5098a4254344d91a630ea5e37a376427672a
Pavel Varchenko <varpa89@gmail.com>
2013-07-27 22:04:14 +0400

    TaskA complete

---
+++ codeforces/src/main/java/ru/varpa89/codeforces/round192Div2/TaskC.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.varpa89.codeforces.round192Div2;
+
+/**
+ *
+ * @author Varchenko http://codeforces.ru/contest/330/problem/C
+ */
+public class TaskC {
+    
+}
